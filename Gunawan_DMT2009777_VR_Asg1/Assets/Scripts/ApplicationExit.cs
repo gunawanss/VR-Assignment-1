3d9e3eb7a292dd778aa993c8f4aac142c16e44a9
gunawanss <yanti80shie@Gmail.com>
2023-05-07 18:36:52 +0800

    final

---
+++ Gunawan_DMT2009777_VR_Asg1/Assets/Scripts/ApplicationExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationExit : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Exit successfully");
+        Application.Quit();
+    }
+}
